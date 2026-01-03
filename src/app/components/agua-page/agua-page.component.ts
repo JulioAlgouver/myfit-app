abb3d25936cb16086a0a1dbbc7c00ff36794848d
Julio Algouver <julioalgouver@gmail.com>
2026-01-03 16:42:43 -0300

    1.0

--- src/app/components/agua-page/agua-page.component.ts
+++ src/app/components/agua-page/agua-page.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrl: './agua-page.component.scss'
 })
 export class AguaPageComponent {
+  public meta: number = 3000;
+  public aguaConsumida: number = 1200;
+  public percentualAtingido: number = 0;
 
+  constructor(){
+    this.calcularPercentualAtingido();
+  }
+
+  public calcularPercentualAtingido(){
+    this.percentualAtingido = (this.aguaConsumida/this.meta)*100;
+  }
+
+  acrescenta200ml(){
+    this.aguaConsumida += 200;
+    this.calcularPercentualAtingido();
+  }
+
+  acrescenta350ml(){
+    this.aguaConsumida += 350;
+    this.calcularPercentualAtingido();
+  }
+
+  acrescenta600ml(){
+    this.aguaConsumida += 600;
+    this.calcularPercentualAtingido()
+  }
+
+  acrescenta1000ml(){
+    this.aguaConsumida += 1000;
+    this.calcularPercentualAtingido();
+  }
+
+  acrescenta1500ml(){
+    this.aguaConsumida += 1500;
+    this.calcularPercentualAtingido();
+  }
+
+  acrescenta2000ml(){
+    this.aguaConsumida += 2000;
+    this.calcularPercentualAtingido();
+  }
 }
