11252f6d8b0006c4e940b312beaf97d1b205e72e
Julio Algouver <julioalgouver@gmail.com>
2026-01-07 17:48:01 -0300

    1.0

--- src/app/components/agua-page/agua-page.component.ts
+++ src/app/components/agua-page/agua-page.component.ts
@@ -1,23 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-agua-page',
   templateUrl: './agua-page.component.html',
   styleUrl: './agua-page.component.scss'
 })
-export class AguaPageComponent {
+export class AguaPageComponent implements OnInit{
   public meta: number = 3000;
   public aguaConsumida: number = 1200;
   public percentualAtingido: number = 0;
 
-  constructor(){
-    this.calcularPercentualAtingido();
+  form!:FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.calcularPercentualAtingido();  
+  }
+
+  ngOnInit():void{
+    this.form = this.fb.group({
+      quantidade:[200]
+    });
   }
 
   public calcularPercentualAtingido(){
     this.percentualAtingido = (this.aguaConsumida/this.meta)*100;
   }
 
+  beberAgua(){
+    const valorSelecionado = this.form.value.quantidade;
+    this.aguaConsumida += Number(valorSelecionado);
+    this.calcularPercentualAtingido();
+  }
+
+  /*
   acrescenta200ml(){
     this.aguaConsumida += 200;
     this.calcularPercentualAtingido();
@@ -47,4 +63,6 @@ export class AguaPageComponent {
     this.aguaConsumida += 2000;
     this.calcularPercentualAtingido();
   }
+
+  */
 }
