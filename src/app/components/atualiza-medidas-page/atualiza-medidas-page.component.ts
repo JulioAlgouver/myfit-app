c6888d7fa1692a8988a5a229e1299abc97e47d7a
Julio Algouver <julioalgouver@gmail.com>
2026-01-09 13:21:17 -0300

    1.0

--- src/app/components/atualiza-medidas-page/atualiza-medidas-page.component.ts
+++ src/app/components/atualiza-medidas-page/atualiza-medidas-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-atualiza-medidas-page',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './atualiza-medidas-page.component.scss'
 })
 export class AtualizaMedidasPageComponent {
+  formMedidas!:FormGroup;
 
+  constructor(public fb:FormBuilder){
+    this.formMedidas = this.fb.group({
+      quadril:['', Validators.required],
+      umbigo:['',Validators.required],
+      cintura:['',Validators.required],
+      braco:['',Validators.required],
+      coxa:['',Validators.required],
+      altura:['',Validators.required]
+    })
+  }
 }
